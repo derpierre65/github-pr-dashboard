78b812a78f118995a8ae2d61cfff7ba97f253558
derpierre65 <git@derpierre65.de>
2025-10-17 11:42:33 +0100

    feat: remove persisted storage requirement

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,11 @@
   <q-layout view="lHh Lpr lff">
     <q-page-container>
       <router-view v-if="hasAccess" class="q-pb-lg" />
+      <q-page v-else-if="error" class="flex flex-center">
+        <q-banner class="bg-red-10">
+          {{ error }}
+        </q-banner>
+      </q-page>
     </q-page-container>
 
     <q-footer class="tw:h-12 q-pa-sm tw:bg-neutral-900!" bordered>
@@ -56,59 +61,59 @@ const dbStore = useDatabaseStore();
 
 const hasAccess = ref(false);
 const updateAvailable = ref(false);
+const error = ref('');
 
 function refreshApplication() {
   window.location.reload();
 }
 
 window.setTimeout(() => {
-  if (hasAccess.value) {
+  if (hasAccess.value || error.value) {
     return;
   }
 
   Loading.show({
     group: 'opening',
-    message: 'Please enable persistent storage, to use this page.',
+    message: 'Creating database...',
   });
 }, 500);
 
-window.navigator.storage.persist().then((granted) => {
-  if (!granted) {
-    return;
+const databaseRequest = indexedDB.open('github_pr_overview', 4);
+
+databaseRequest.onupgradeneeded = (event: Event) => {
+  const db = event.target!.result as IDBDatabase;
+
+  if (!db.objectStoreNames.contains('pull_requests')) {
+    db.createObjectStore('pull_requests', {
+      keyPath: 'id',
+    });
   }
+  if (!db.objectStoreNames.contains('repositories')) {
+    const store = db.createObjectStore('repositories', {
+      keyPath: 'repository',
+    });
+    store.createIndex('repository', 'repository', {
+      unique: true,
+    });
+  }
+  if (!db.objectStoreNames.contains('filters')) {
+    db.createObjectStore('filters', {
+      keyPath: 'id',
+      autoIncrement: true,
+    });
+  }
+};
+databaseRequest.onsuccess = (event: Event) => {
+  dbStore.db = event.target!.result as IDBDatabase;
+  hasAccess.value = true;
+  Loading.hide('opening');
+};
+databaseRequest.onerror = () => {
+  error.value = 'Can\t create database.';
+  Loading.hide('opening');
+};
 
-  const databaseRequest = indexedDB.open('github_pr_overview', 4);
-
-  databaseRequest.onupgradeneeded = (event: Event) => {
-    const db = event.target!.result as IDBDatabase;
-
-    if (!db.objectStoreNames.contains('pull_requests')) {
-      db.createObjectStore('pull_requests', {
-        keyPath: 'id',
-      });
-    }
-    if (!db.objectStoreNames.contains('repositories')) {
-      const store = db.createObjectStore('repositories', {
-        keyPath: 'repository',
-      });
-      store.createIndex('repository', 'repository', {
-        unique: true,
-      });
-    }
-    if (!db.objectStoreNames.contains('filters')) {
-      db.createObjectStore('filters', {
-        keyPath: 'id',
-        autoIncrement: true,
-      });
-    }
-  };
-
-  databaseRequest.onsuccess = (event: Event) => {
-    dbStore.db = event.target!.result as IDBDatabase;
-    hasAccess.value = true;
-    Loading.hide('opening');
-  };
-});
+window.navigator.storage.persist();
 
 if (!import.meta.env.DEV) {
   useInterval().registerInterval(() => {
