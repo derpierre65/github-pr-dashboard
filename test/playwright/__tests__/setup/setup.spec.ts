30c8bb7f9194a0a7c619753c544bd5ec981d566d
derpierre65 <git@derpierre65.de>
2025-11-17 23:31:18 +0000

    test: add waitForURL

--- test/playwright/__tests__/setup/setup.spec.ts
+++ test/playwright/__tests__/setup/setup.spec.ts
@@ -2,6 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('should redirect to setup page and setup title should be visible', async({ page, }) => {
   await page.goto('/');
+
+  // wait for redirect to /setup for new users
+  await page.waitForURL('/setup');
+
   await expect(page.getByTestId('setup-title')).toHaveText('Welcome to GitHub Pull Request Dashboard');
-  await expect(page).toHaveURL('/setup');
 });
